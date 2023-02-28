4ffea3f1ba091466acdda7236a114a2074367c07
Lee Jordan <duracell80@users.noreply.github.com>
2023-02-28 05:57:25 -0600

    Adding IPTV Install and scan

--- install-iptv.sh
+++ install-iptv.sh
@@ -21,7 +21,29 @@ for filename in $CWD/*.desktop; do
 done
 
 # Install Hypnotix 2.0
+mkdir -p $CWD/deps
+cd $CWD/deps
+
+wget -nc https://ftp5.gwdg.de/pub/linux/debian/mint/packages/pool/main/h/hypnotix/hypnotix_2.0_all.deb && sudo dpkg -i hypnotix_2.0_all.deb 
+#rm -f $CWD/deps/hypnotix_2.0_all.deb
+
+LWD=$HOME/.local/share/powertoys
+LBD=$HOME/.local/bin
+
+git clone https://github.com/Silicondust/libhdhomerun.git
+cd $CWD/deps/libhdhomerun
+make
+cp $CWD/deps/libhdhomerun/hdhomerun_config $LBD
+cd $CWD/scripts/
+clear
+
+LWD=$HOME/.local/share/powertoys
+mkdir -p $LWD
 cd $CWD
-wget https://ftp5.gwdg.de/pub/linux/debian/mint/packages/pool/main/h/hypnotix/hypnotix_2.0_all.deb && sudo dpkg -i hypnotix_2.0_all.deb 
-rm -f $CWD/hypnotix_2.0_all.deb
+cp -f $CWD/scripts/hdhr_channels.py $LWD
+cp -f $CWD/scripts/hypnotix_hdhr.sh $LWD
+
+
+
+$LWD/hypnotix_hdhr.sh
 
---
+++ scripts/hypnotix_hdhr.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# https://forums.linuxmint.com/viewtopic.php?t=304391
+# provider default: ['Free-TV:::url:::https://raw.githubusercontent.com/Free-TV/IPTV/master/playlist.m3u8:::::::::']
+# provider additional: 'HDHomerun:::local:::file:///~/.local/share/powertoys/iptv-hd-homerun.m3u:::::::::'
+
+MSG_0=0
+
+LWD="$HOME/.local/share/powertoys"
+CWD="$HOME/.cache/powertoys"
+HWD="$HOME/.cache/hypnotix"
+
+mkdir -p $CWD
+mkdir -p $HWD
+mkdir -p $HWD/providers
+
+
+$LWD/hdhr_channels.py
+
+# COPY FROM TUNER CACHED M3U
+cp -f $HWD/providers/hd-homerun $LWD/iptv-hd-homerun.m3u
+cp -f $HWD/providers/hd-homerun $CWD/iptv-hd-homerun.m3u
+
+HYP_GET=$(gsettings get org.x.hypnotix providers)
+
+# SET AS HYPNOTIX PROVIDER
+if [[ $HYP_GET == *"homerun"* ]]; then
+    exit
+else
+    HYP_SET=$(gsettings get org.x.hypnotix providers | sed "s|:']|:', 'HDHomeRun:::local:::file://${HOME}/.local/share/powertoys/iptv-hd-homerun.m3u:::::::::']|" | uniq)
+
+    gsettings set org.x.hypnotix providers "${HYP_SET}"
+fi
