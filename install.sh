ae9a3f86f8b69071d0e4692bc661f7b3d8e3545b
Lee Jordan <duracell80@users.noreply.github.com>
2023-02-27 22:08:27 -0600

    Create autostart

--- install.sh
+++ install.sh
@@ -18,3 +18,16 @@ cp -rf $CWD/icons/Black-Icons/* $DIR_TEMP/icons/dark
 
 cp -rf $CWD/icons/White-Icons/* $DIR_ICON/light
 cp -rf $CWD/icons/Black-Icons/* $DIR_ICON/dark
+
+cp $CWD/remix-icons.sh $HOME/.local/share/zorin-touchup/
+chmod u+x $HOME/.local/share/zorin-touchup/*.sh
+
+# SET ANY AUTOSTART SCRIPTS FOR DESKTOP ENVIRONMENT
+for filename in $CWD/*.desktop; do
+    [ -e "$filename" ] || continue
+    file=$(echo $filename | sed -e "s|${CWD}/||g")
+
+    cp -f "$filename" "$file.tmp"
+    sed -i "s|Exec=~/|Exec=$HOME/|g" "$file.tmp"
+    mv -f "$file.tmp" "$HOME/.config/autostart/$file"
+done
--- remix-icons.sh
+++ remix-icons.sh
@@ -2,8 +2,6 @@
 
 DIR_BASE=/usr/share/themes
 DIR_NAME=$(gsettings get org.gnome.desktop.interface gtk-theme | sed "s/'//g")
-DIR_FILE_GTK=$DIR_BASE/$DIR_NAME/gtk-3.0/gtk.css
-
 DIR_TEMP=$HOME/.cache/zorin-touchup
 DIR_ICON=$HOME/.local/share/zorin-touchup/icons
 DIR_REAL_LIGHT=$HOME/.local/share/icons/Light-Icons
@@ -16,63 +14,84 @@ mkdir -p $DIR_ICON/dark
 mkdir -p $DIR_REAL_LIGHT
 mkdir -p $DIR_REAL_DARK
 
-cp -rf $DIR_ICON/light $DIR_TEMP/icons
-cp -rf $DIR_ICON/dark $DIR_TEMP/icons
+while :
+do
 
-THEME_COLOR_EG=$(cat $DIR_FILE_GTK | grep -i "define-color theme_unfocused_fg_color" | cut -d ' ' -f3 | sed "s/;//g")
-THEME_COLOR_FG=$(cat $DIR_FILE_GTK | grep -i "define-color theme_fg_color" | cut -d ' ' -f3 | sed "s/;//g")
-THEME_COLOR_BG=$(cat $DIR_FILE_GTK | grep -i "define-color theme_bg_color" | cut -d ' ' -f3 | sed "s/;//g")
+	CUR_NAME=$(gsettings get org.gnome.desktop.interface gtk-theme | sed "s/'//g")
 
-echo "FG: ${THEME_COLOR_FG} BG: ${THEME_COLOR_BG} EG: ${THEME_COLOR_EG}"
+	if [[ $CUR_NAME == $DIR_NAME ]]; then
+		ACT="0"
+	else
+		ACT="1"
+		sleep 5
+		CUR_NAME=$(gsettings get org.gnome.desktop.interface gtk-theme | sed "s/'//g")
+		echo "[i] Theme changed ${CUR_NAME}"
 
-FILE_NME=$DIR_TEMP/list.txt
-FILE_TOP=$DIR_TEMP/list_top.txt
+		DIR_FILE_GTK=$DIR_BASE/$CUR_NAME/gtk-3.0/gtk.css
+		DIR_FILE_GSH=$DIR_BASE/$CUR_NAME/gnome-shell/gnome-shell.css
 
-# WHITE ICONS
-ls $DIR_TEMP/icons/light/scalable/ > $FILE_TOP
-while read -r LINE; do
-	ls $DIR_TEMP/icons/light/scalable/$LINE/*.svg > $FILE_NME
-	while read -r LINE; do
-		sed -i 's|#cccccc|'${THEME_COLOR_FG}'|g' ${LINE}
-		sed -i 's|#ffaa00|'${THEME_COLOR_EG}'|g' ${LINE}
+		cp -rf $DIR_ICON/light $DIR_TEMP/icons
+		cp -rf $DIR_ICON/dark $DIR_TEMP/icons
 
-		sed -i "s|fill:#cccdce|fill:${THEME_COLOR_EG}|g" ${LINE}
-		sed -i "s|stop-color:#ddd|stop-color:${THEME_COLOR_FG}|g" ${LINE}
-    		sed -i "s|stop-color:#fff|stop-color:${THEME_COLOR_FG}|g" ${LINE}
-		sed -i "s|fill:#707073|fill:${THEME_COLOR_BG}|g" ${LINE}
-	done < $FILE_NME
-	rm -f $FILE_NME
-done < $FILE_TOP
-rm -f $FILE_TOP
+		THEME_COLOR_EG=$(cat $DIR_FILE_GTK | grep -i "define-color theme_unfocused_fg_color" | cut -d ' ' -f3 | sed "s/;//g")
+		THEME_COLOR_FG=$(cat $DIR_FILE_GTK | grep -i "define-color theme_fg_color" | cut -d ' ' -f3 | sed "s/;//g")
+		THEME_COLOR_BG=$(cat $DIR_FILE_GTK | grep -i "define-color theme_bg_color" | cut -d ' ' -f3 | sed "s/;//g")
+		THEME_COLOR_AC=$(cat $DIR_FILE_GSH  | grep -i "popup-menu-item:checked" | head -n1 | cut -d ';' -f3 | cut -d ':' -f2 | sed "s/ //g")
 
-# BLACK ICONS
-ls $DIR_TEMP/icons/dark/scalable/ > $FILE_TOP
-while read -r LINE; do
-        ls $DIR_TEMP/icons/dark/scalable/$LINE/*.svg > $FILE_NME
-        while read -r LINE; do
-                sed -i 's|#000000|'${THEME_COLOR_FG}'|g' ${LINE}
-                sed -i 's|#ffaa00|'${THEME_COLOR_EG}'|g' ${LINE}
+		echo "FG: ${THEME_COLOR_FG} BG: ${THEME_COLOR_BG} EG: ${THEME_COLOR_EG} AC: ${THEME_COLOR_AC}"
 
-		sed -i "s|fill:#999|fill:${THEME_COLOR_EG}|g" ${LINE}
-                sed -i "s|stop-color:#000|stop-color:${THEME_COLOR_FG}|g" ${LINE}
-                sed -i "s|stop-color:#666|stop-color:${THEME_COLOR_FG}|g" ${LINE}
-                sed -i "s|fill:#000000|fill:${THEME_COLOR_BG}|g" ${LINE}
-        done < $FILE_NME
-        rm -f $FILE_NME
-done < $FILE_TOP
-rm -f $FILE_TOP
+		FILE_NME=$DIR_TEMP/list.txt
+		FILE_TOP=$DIR_TEMP/list_top.txt
 
-# COPY OVER TO THE REAL DIRECTORY
-cp -rf $DIR_TEMP/icons/light/* $DIR_REAL_LIGHT
-cp -rf $DIR_TEMP/icons/dark/* $DIR_REAL_DARK
+		# WHITE ICONS
+		ls $DIR_TEMP/icons/light/scalable/ > $FILE_TOP
+		while read -r LINE; do
+			ls $DIR_TEMP/icons/light/scalable/$LINE/*.svg > $FILE_NME
+			while read -r LINE; do
+				sed -i 's|#cccccc|'${THEME_COLOR_FG}'|g' ${LINE}
+				sed -i 's|#ffaa00|'${THEME_COLOR_EG}'|g' ${LINE}
 
+				sed -i "s|fill:#cccdce|fill:${THEME_COLOR_EG}|g" ${LINE}
+				sed -i "s|stop-color:#ddd|stop-color:${THEME_COLOR_FG}|g" ${LINE}
+		    		sed -i "s|stop-color:#fff|stop-color:${THEME_COLOR_FG}|g" ${LINE}
+				sed -i "s|fill:#707073|fill:${THEME_COLOR_BG}|g" ${LINE}
+			done < $FILE_NME
+			rm -f $FILE_NME
+		done < $FILE_TOP
+		rm -f $FILE_TOP
 
+		# BLACK ICONS
+		ls $DIR_TEMP/icons/dark/scalable/ > $FILE_TOP
+		while read -r LINE; do
+			ls $DIR_TEMP/icons/dark/scalable/$LINE/*.svg > $FILE_NME
+			while read -r LINE; do
+				sed -i 's|#000000|'${THEME_COLOR_FG}'|g' ${LINE}
+				sed -i 's|#ffaa00|'${THEME_COLOR_FG}'|g' ${LINE}
 
-# REFRESH ICONS
-gsettings set org.gnome.desktop.interface icon-theme "Hicolor"
-sleep 1
-if [[ $DIR_NAME == *"Dark"* ]]; then
-	gsettings set org.gnome.desktop.interface icon-theme "Light-Icons"
-else
-	gsettings set org.gnome.desktop.interface icon-theme "Dark-Icons"
-fi
+				sed -i "s|fill:#999|fill:${THEME_COLOR_FG}|g" ${LINE}
+				sed -i "s|stop-color:#000|stop-color:${THEME_COLOR_AC}|g" ${LINE}
+				sed -i "s|stop-color:#666|stop-color:${THEME_COLOR_AC}|g" ${LINE}
+				sed -i "s|fill:#000000|fill:${THEME_COLOR_FG}|g" ${LINE}
+			done < $FILE_NME
+			rm -f $FILE_NME
+		done < $FILE_TOP
+		rm -f $FILE_TOP
+
+		# COPY OVER TO THE REAL DIRECTORY
+		cp -rf $DIR_TEMP/icons/light/* $DIR_REAL_LIGHT
+		cp -rf $DIR_TEMP/icons/dark/* $DIR_REAL_DARK
+
+		# REFRESH ICONS
+		gsettings set org.gnome.desktop.interface icon-theme "Hicolor"
+		sleep 1
+		if [[ $CUR_NAME == *"Dark"* ]]; then
+			gsettings set org.gnome.desktop.interface icon-theme "Light-Icons"
+		else
+			gsettings set org.gnome.desktop.interface icon-theme "Dark-Icons"
+		fi
+
+		DIR_NAME=$(gsettings get org.gnome.desktop.interface gtk-theme | sed "s/'//g")
+	fi
+
+sleep 2
+done
