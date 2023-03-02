2935b8ed0cb7e82877db63f6bb6ee3861b10a848
Lee Jordan <duracell80@users.noreply.github.com>
2023-03-02 00:33:45 -0600

    Add more extensions

--- install-extensions.sh
+++ install-extensions.sh
@@ -5,11 +5,13 @@ CWD=$(pwd)
 DIR_TEMP=$CWD/extensions
 DIR_EXTT=$HOME/.local/share/gnome-shell/extensions
 
+sudo apt install chrome-gnome-shell gnome-shell-extension-prefs
+
 mkdir -p $DIR_EXTT
 cp -rf $DIR_TEMP/expandable-notifications/expandable-notifications@kaan.g.inam.org $DIR_EXTT
 cp -rf $DIR_TEMP/weather-oclock/weatheroclock@CleoMenezesJr.github.io $DIR_EXTT
+cp -rf $DIR_TEMP/clocks/unix-timestamp-clock@se1exin.github.com $DIR_EXTT
+cp -rf $DIR_TEMP/bing-wallpaper/BingWallpaperineffable-gmail.com.v40.shell-extension $DIR_EXTT
 
+gnome-shell --version
 gnome-extensions list --user
-
-gnome-extensions enable expandable-notifications@kaan.g.inam.org
-gnome-extensions enable weatheroclock@CleoMenezesJr.github.io
