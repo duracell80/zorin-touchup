7c191d968d403fc164519165f5fafa5944836786
Lee Jordan <duracell80@users.noreply.github.com>
2023-03-03 21:25:52 -0600

    Improve BingPaper install

--- install-extensions.sh
+++ install-extensions.sh
@@ -5,13 +5,18 @@ CWD=$(pwd)
 DIR_TEMP=$CWD/extensions
 DIR_EXTT=$HOME/.local/share/gnome-shell/extensions
 
-sudo apt install chrome-gnome-shell gnome-shell-extension-prefs gnome-weather
+sudo apt install chrome-gnome-shell gnome-shell-extension-prefs gnome-weather intltool
 
 mkdir -p $DIR_EXTT
 cp -rf $DIR_TEMP/expandable-notifications/expandable-notifications@kaan.g.inam.org $DIR_EXTT
 cp -rf $DIR_TEMP/clocks/weatheroclock@CleoMenezesJr.github.io $DIR_EXTT
 cp -rf $DIR_TEMP/clocks/unix-timestamp-clock@se1exin.github.com $DIR_EXTT
-cp -rf $DIR_TEMP/bing-wallpaper/BingWallpaperineffable-gmail.com.v40.shell-extension $DIR_EXTT
+#cp -rf $DIR_TEMP/bing-wallpaper/BingWallpaperineffable-gmail.com.v40.shell-extension $DIR_EXTT
+
+cd $DIR_TEMP/bing-wallpaper/bing-wallpaper-gnome-extension
+$DIR_TEMP/bing-wallpaper/bing-wallpaper-gnome-extension/install.sh
+
+cd $CWD
 
 gnome-shell --version
 gnome-extensions list --user
